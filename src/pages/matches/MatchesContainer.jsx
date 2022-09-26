591c1a545b2de6dbdc13570e68eed4a9ba078607
Sebastian Ramos <sebastianalejandroramos96@gmail.com>
2022-09-26 20:12:02 -0300

    22092022

--- src/pages/matches/MatchesContainer.jsx
+++ src/pages/matches/MatchesContainer.jsx
@@ -20,14 +20,12 @@ function MatchesContainer() {
   return (
     <div className='container matches-container'>
       <div className='row'>
-        
+      {matchCtx.matchesList.filter(mat => mat.date == idParamMatch).map(d => <img alt="" src={d.img}/>)}
       </div>
       <div className='row'>
-        
-       
+      <h3>Partido contra {matchCtx.matchesList.filter(mat => mat.date == idParamMatch).map(d => <p>{d.rival}</p>)}</h3>
       </div>
       <div className='row'>
-       
         {matchCtx.matchesList.filter(mat => mat.date == idParamMatch).map(d => <StatsTable props={d}/>)}
       </div>
     </div>
